f0d2f8c510f05e4c781a27fbaac0a88a0f28d55f
Pavel Shustov <macbookair@MacBook-Air-MacBook.local>
2020-07-09 23:40:30 +0500

    Added some comments co code

--- Dictonary/dict.py
+++ Dictonary/dict.py
@@ -10,14 +10,14 @@ def read_cvs():
         return Dictorany
 
 def guees_artikel(Dictorany):
-    random.shuffle(Dictorany)
-    counter = 0
+    random.shuffle(Dictorany)  # Sorts list randomly 
+    counter = 0                # Setting counter for correct answers to 0
     for word in range(0, len(Dictorany)-1):
         answer = input("What's the Artikel for word {0} ? \n".format(Dictorany[word][1]))
         if answer == Dictorany[word][0]:
             print("Good! \n")
-            counter += 1
-        elif answer == 0:
+            counter += 1      # If answered correctly, increase counter 
+        elif answer == 0:     # Point of exit
             break
         else:
             print("Incorrect! The right atrikel ist {0}! \n".format(Dictorany[word][0]))
